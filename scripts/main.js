cea0d8a48e1c501fe692e0112ea3b0b01e483126
James Campbell <jamesrcampbell.obb@gmail.com>
2024-06-13 14:22:34 -0500

    I have created the main js file to push our component html to DOM

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+import { Areas } from "./Areas.js"
+
+
+
+const mainContainer = document.querySelector("#container")
+
+const applicationHTML = `
+<h1>Cider Falls</h1>
+<article class="details">
+    <section class="detail--column list details__areas">
+        <h2>Park Areas</h2>
+        ${Areas()}
+    </section>
+    <section class="detail--column list details__guests">
+        <h2>Current Guests</h2>
+        Guests
+    </section>
+`
+
+mainContainer.innerHTML = applicationHTML
