442a5b71e0022b82f35e28c9807dab61ec23d96a
James Campbell <jamesrcampbell.obb@gmail.com>
2024-06-13 13:30:00 -0500

    I have created the database module and getter functions for use in our team project

---
+++ scripts/database.js
@@ -0,0 +1,81 @@
+
+const database = {
+    parkAreas: [{
+        id: 1,
+        name: "Chamfort River",
+    },{
+        id: 2,
+        name: "Lost Wolf Hiking Trail",
+    },{
+        id: 3,
+        name: "Lodge",
+    },{
+        id: 4,
+        name: "Gander River",
+    },{
+        id: 5,
+        name: "Campgrounds",
+    },{
+        id: 6,
+        name: "Pine Bluffs Trails",
+    },],
+    services: [{
+        id: 1,
+        name:  "rafting",
+    },{
+        id: 2,
+        name: "conoeing",
+    },{
+        id: 3,
+        name: "fishing",
+    },{
+        id: 4,
+        name: "hiking",
+    },{
+        id: 5,
+        name: "picnicking",
+    },{
+        id: 6,
+        name: "rock climbing",
+    },{
+        id: 7,
+        name: "lodging",
+    },{
+        id: 8,
+        name: "parking",
+    },{
+        id: 9,
+        name: "information",
+    },{
+        id: 10,
+        name: "zip lines",
+    },{
+        id: 11,
+        name: "hotel",
+    },{
+        id: 12,
+        name: "restaurant",
+    },{
+        id: 13,
+        name: "office park",
+    },{
+        id: 14,
+        name: "children's play areas",
+    },{
+        id: 15,
+        name: "food vendors",
+    },],
+    guests: [{
+        id: ,
+        name: ,
+        parkAreaId: ,
+    },]
+}
+
+export const getAreas
+
+export const getServices
+
+export const getGuests
+
+//rafting, conoeing, fishing, hiking, picnicking, rock climbing, lodging, parking, information, zip lines, hotel, restaurant, office park, children play areas, food vendors
